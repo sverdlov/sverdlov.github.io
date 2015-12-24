73a72ede102c32cdfd9ed64805dc4433c758887f
sverdlov <naremo133@gmail.com>
2015-12-24 11:29:43 +0200

    add kitchen

---
+++ sites/kitchen/js/main.js
@@ -0,0 +1,16 @@
+var options = {
+  horizontal: 1,
+  itemNav: 'basic',
+  activateOn: 'click',
+  scrollBy: 1,
+  mouseDragging: 1,
+  touchDragging: 1,
+  releaseSwing: 1,
+  scrollBar: $('.scrollbar'),
+  speed: 300,
+  elasticBounds: 1,
+  dragHandle: 1,
+  clickBar: 1
+};
+$('.frame').sly(options);
+
