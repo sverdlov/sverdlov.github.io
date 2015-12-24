c9ab755f90c3707c8ae840cc3abe79a340a3c804
sverdlov <naremo133@gmail.com>
2015-12-24 11:22:41 +0200

    add emmeline

---
+++ sites/emmeline/js/main.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+		$('.slide_wrapp').slick({
+			autoplay: true,
+			autoplaySpeed: 5000,
+			infinite: true,
+			dots: true,
+			speed: 1000,
+			prevArrow: '.prev',
+			nextArrow: '.next',
+		});
+	});
+
+$(document).ready(function () {
+		$('.t-slider').slick({
+			autoplay: true,
+			autoplaySpeed: 5000,
+			infinite: true,
+			arrows: false,
+			speed: 1000,
+			dots: true,
+		});
+	});	
